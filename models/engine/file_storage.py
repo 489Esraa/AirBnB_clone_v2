a0b0fd581ae0c3f25b8a7b676e0631a0e560d304
489Esraa <asraaalaa2050@gmail.com>
2024-04-21 04:34:32 +0200

    esraaalaa

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -5,7 +5,8 @@ import json
 
 class FileStorage:
     """This class manages storage of hbnb models in JSON format"""
-    __file_path = 'file.json'
+
+    __file_path = "file.json"
     __objects = {}
 
     def all(self):
@@ -14,11 +15,11 @@ class FileStorage:
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+        self.all().update({obj.to_dict()["__class__"] + "." + obj.id: obj})
 
     def save(self):
         """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
+        with open(FileStorage.__file_path, "w") as f:
             temp = {}
             temp.update(FileStorage.__objects)
             for key, val in temp.items():
@@ -36,18 +37,23 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-    }
+            "BaseModel": BaseModel,
+            "User": User,
+            "Place": Place,
+            "State": State,
+            "City": City,
+            "Amenity": Amenity,
+            "Review": Review,
+        }
         try:
             temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
+            with open(FileStorage.__file_path, "r") as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val["__class__"]](**val)
         except FileNotFoundError:
             pass
+
     def delete(self, obj=None):
         """Delete object """
         cs_key = ""
@@ -55,4 +61,14 @@ class FileStorage:
             for key, value in self.__objects.items():
                 if value.id == obj.id:
                     cs_key = key
-            del self.__objects[cs_key]      
+            del self.__objects[cs_key]
+
+    def all(self, cls=None):
+        """ that returns the list of objects of one type of class"""
+        object = {}
+        if cls is not None:
+            for k, v in FileStorage.__objects.items():
+                if isinstance(v, cls):
+                    object[k] = v
+            return object
+        return FileStorage.__objects
