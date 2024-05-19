d6c208a4aa6ab1642d99aa75cb08ffdbc2501b42
489Esraa <asraaalaa2050@gmail.com>
2024-05-19 18:58:06 +0300

    esraaalaa

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
 """ module doc """
+#  script that starts a Flask web application:
+#Your web application must be listening on 0.0.0.0, port 5000
+#Routes:
+#/: display “Hello HBNB!”
+#You must use the option strict_slashes=False in your route definition
+#strict_slashes=False parameter allows the route to be accessed with or without a trailing slash.
+#function Flask_web_app ():Define the Route Handler is defined to handle requests to the /
 from flask import Flask
 
 app = Flask(__name__)
